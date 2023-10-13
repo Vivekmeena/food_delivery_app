21c39422d853932f52db4b3ecf28951254a32c01
Vivekmeena <vivekmeena182000@gmail.com>
2023-10-13 10:46:18 +0530

    updating the code

--- backend/db.js
+++ backend/db.js
@@ -1,35 +1,13 @@
 const mongoose = require('mongoose')
-// const mongoDbClient = require("mongodb").MongoClient
-const mongoURI = 'mongodb+srv://vivekmeena182000:jaishreeram@cluster0.0h84e7a.mongodb.net/GoFood?retryWrites=true&w=majority' // Customer change url to your db you created in atlas
-// mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
-// module.exports = function (callback) {
-//     mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
-//         // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
-//         if (err) console.log("---" + err)
-//         else {
-//             // var database =
-//             console.log("connected to mongo")
-//             const foodCollection = await mongoose.connection.db.collection("food_items");
-//             foodCollection.find({}).toArray(async function (err, data) {
-//                 const categoryCollection = await mongoose.connection.db.collection("Categories");
-//                 categoryCollection.find({}).toArray(async function (err, Catdata) {
-//                     callback(err, data, Catdata);
 
-//                 })
-//             });
-//             // listCollections({name: 'food_items'}).toArray(function (err, database) {
-//             // });
-//             //     module.exports.Collection = database;
-//             // });
-//         }
-//     })
-// };
-
-
+const mongoURI ='mongodb+srv://foodapp:Foodie123@cluster0.t1593vh.mongodb.net/foodapp?retryWrites=true&w=majority'
+//'mongodb+srv://foodapp:Foodie123@cluster0.t1593vh.mongodb.net/?retryWrites=true&w=majority'
+//'mongodb+srv://vivekmeena182000:jaishreeram@cluster0.0h84e7a.mongodb.net/GoFood?retryWrites=true&w=majority' // Customer change url to your db you created in atlas
 
 module.exports = async function (callback) {
     try {
-        await mongoose.connect('mongodb+srv://vivekmeena182000:jaishreeram@cluster0.0h84e7a.mongodb.net/GoFood?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://foodapp:Foodie123@cluster0.t1593vh.mongodb.net/foodapp?retryWrites=true&w=majority');
+            //'mongodb+srv://vivekmeena182000:jaishreeram@cluster0.0h84e7a.mongodb.net/GoFood?retryWrites=true&w=majority');
 
         console.log("Connected to MongoDB");
 
@@ -46,3 +24,5 @@ module.exports = async function (callback) {
         callback(err);
     }
 };
+
+//'mongodb+srv://vivekmeena182000:jaishreeram@cluster0.0h84e7a.mongodb.net/GoFood?retryWrites=true&w=majority'
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -36,7 +36,7 @@ export default function Home() {
         <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel">
 
           <div className="carousel-inner " id='carousel'>
-            <div class=" carousel-caption  " style={{ zIndex: "9" }}>
+            <div className=" carousel-caption  " style={{ zIndex: "9" }}>
               <div className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
                 <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search in here..." aria-label="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
                 <button className="btn text-white bg-danger" onClick={() => { setSearch('') }}>X</button>
@@ -91,93 +91,5 @@ export default function Home() {
       <Footer />
     </div>
 
-
-
-
-
-
-
-
-
   )
 }
-
-
-// import React, { useEffect, useState } from 'react'
-// import Card from '../components/Card'
-// // import Carousel from '../components/Carousel'
-// import Footer from '../components/Footer'
-// import Navbar from '../components/Navbar'
-
-// export default function Home() {
-//   const [foodCat, setFoodCat] = useState([])
-//   const [foodItems, setFoodItems] = useState([])
-//   const [search, setSearch] = useState('')
-
-//   const loadFoodItems = async () => {
-//     try {
-//       const response = await fetch("http://localhost:5000/api/auth/foodData", {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//       })
-//       const data = await response.json()
-//       setFoodItems(data[0])
-//       setFoodCat(data[1])
-//     } catch (error) {
-//       console.error(error)
-//     }
-//   }
-
-//   useEffect(() => {
-//     loadFoodItems()
-//   }, [])
-
-//   return (
-//     <div>
-//       <div>
-//         <Navbar />
-//       </div>
-//       <div>
-//         <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel">
-//           <div className="carousel-inner " id='carousel'>
-//             <div className=" carousel-caption  " style={{ zIndex: "9" }}>
-//               <div className=" d-flex justify-content-center">
-//                 <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search in here..." aria-label="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
-//                 <button className="btn text-white bg-danger" onClick={() => { setSearch('') }}>X</button>
-//               </div>
-//             </div>
-//             <div className="carousel-item active" >
-//               <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
-//             </div>
-//             <div className="carousel-item">
-//               <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
-//             </div>
-//             <div className="carousel-item">
-//               <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
-//             </div>
-//           </div>
-//           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-//             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-//             <span className="visually-hidden">Previous</span>
-//           </button>
-//           <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-//             <span className="carousel-control-next-icon" aria-hidden="true"></span>
-//             <span className="visually-hidden">Next</span>
-//           </button>
-//         </div>
-//       </div>
-//       <div className='container'>
-//         {
-//           foodCat !== [] ?
-//             foodCat.map((data) => {
-//               return (
-//                 <div className='row mb-3'>
-//                   <div key={data._id} className='fs-3 m-3'>
-//                     {data.CategoryName}
-//                   </div>
-//                   <hr id="hr-success" style={{ height: "4px"}}/>
-//                   </div>)
-// </div>
-            
